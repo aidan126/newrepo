43c2d23d300bf14fc01d11b82766373ef0538edc
aidan <hkaidan126@gmail.com>
2018-11-08 01:22:43 +0800

    add file isPrime

---
+++ isPrime.js
@@ -0,0 +1,14 @@
+function isPrime(number) {
+  let start = 2;
+  while (start <= Math.sqrt(number)) {
+    if (number % start < 1) {
+      return false;
+    }
+    start++;
+  }
+  return true;
+}
+
+let x = 17;
+console.log(Math.sqrt(x));
+console.log(isPrime(x));
